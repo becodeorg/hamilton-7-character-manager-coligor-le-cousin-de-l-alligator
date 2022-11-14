924fba91f311ce0a4116ead6926c645ea1d540ee
Colin <colin25.huart@hotmail.com>
2022-11-14 09:22:54 +0100

    Merge branch 'development' of github.com:becodeorg/hamilton-7-character-manager-coligor-le-cousin-de-l-alligator into Colin

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import "./style.css";
 import "./mainPage.js";
-=======
-import './style.css'
-import './index.js'
 
->>>>>>> 24a5fc159a8ddd7fbc9a7e2f9aed3b3f49af1828
+import "./index.js";
